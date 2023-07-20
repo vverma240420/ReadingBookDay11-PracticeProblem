68891dbe3c8f01a49c5f9f301bd666efd35d2578
Vivek Verma <vverma240420@gmail.com>
2023-07-20 18:42:56 +0530

    Added Book Reading program

---
+++ ReadingBook/BookReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingBook
+{
+    public class BookReader
+    {
+        public static int CountRemainingChapters(int totalChapters, BookChapter[] chapters, int currentPage)
+        {
+            int remainingChapters = 0;
+            for(int i=0; i<totalChapters; i++) 
+            {
+                if (chapters[i].StartPage<= currentPage && chapters[i].EndPage>=currentPage)
+                {
+                    remainingChapters = totalChapters - i;
+                    break;
+                }
+            }
+            return remainingChapters;
+        }
+    }
+}
---
+++ ReadingBook/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReadingBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalChapters = 4;
+            BookChapter[] chapters = new BookChapter[]
+            {
+                new BookChapter{StartPage = 1, EndPage = 4},
+                new BookChapter{StartPage = 5, EndPage = 7},
+                new BookChapter{StartPage = 9, EndPage = 16},
+                new BookChapter{StartPage = 17, EndPage = 26}
+            };
+            int currentPage = 7;
+            int remainingChapters = BookReader.CountRemainingChapters(totalChapters, chapters, currentPage);
+            Console.WriteLine(remainingChapters);
+            Console.ReadLine();
+        }
+    }
+}
